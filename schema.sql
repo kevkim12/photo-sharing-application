d3d709d7fe35c0b68b34bd7b27a818e1b6f24e0f
kevkimbu <kevkim@bu.edu>
2023-03-01 16:50:11 -0500

    Update schema.sql with new changes

--- schema.sql
+++ schema.sql
@@ -4,10 +4,20 @@ DROP TABLE IF EXISTS Pictures CASCADE;
 DROP TABLE IF EXISTS Users CASCADE;
 
 CREATE TABLE Users (
-    user_id int4  AUTO_INCREMENT,
-    email varchar(255) UNIQUE,
-    password varchar(255),
-  CONSTRAINT users_pk PRIMARY KEY (user_id)
+    user_id int4  AUTO_INCREMENT NOT NULL,
+    first_name varchar(100) NOT NULL,
+    last_name varchar(100) NOT NULL,
+    password varchar(255) NOT NULL,
+    gender varchar(6),
+    email varchar(255) UNIQUE NOT NULL,
+    hometown varchar(255),
+    dob DATE NOT NULL,
+  CONSTRAINT users_pk PRIMARY KEY (user_id),
+  CHECK (first_name IS NOT NULL AND
+         last_name IS NOT NULL AND
+         password IS NOT NULL AND
+         email IS NOT NULL AND
+         dob IS NOT NULL)
 );
 
 CREATE TABLE Pictures
