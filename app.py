fe1c4b39175adb69b466c7d54012c2f2ff1736df
kevkimbu <kevkim@bu.edu>
2023-03-01 21:16:28 -0500

    Login and friends input field require email

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def login():
 	if flask.request.method == 'GET':
 		return '''
 			   <form action='login' method='POST'>
-				<input type='text' name='email' id='email' placeholder='email'></input>
+				<input type='email' name='email' id='email' placeholder='email'></input>
 				<input type='password' name='password' id='password' placeholder='password'></input>
 				<input type='submit' name='submit'></input>
 			   </form></br>
